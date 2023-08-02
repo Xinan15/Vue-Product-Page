19a3f8073fff631e58681173377d806107c9921e
Xinan <youkingan11@gmail.com>
2023-08-02 11:36:57 +0100

    update

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const app = Vue.createApp({
+  // data()
   data() {
     return {
-      image: "./src/assets/images/socks_green.jpg",
       product: "Socks",
+
+      image: "./src/assets/images/socks_green.jpg",
+
       inStock: true,
       inventory: 100,
 
@@ -10,9 +13,32 @@ const app = Vue.createApp({
       details: ["50% cotton", "30% wool", "20% polyester"],
 
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: 'green', image: './src/assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './src/assets/images/socks_blue.jpg' },
       ],
+
+      // initial value of cart is 0
+      cart: 0,
+
+      styles: {
+        color: 'red',
+        fontSize: '20px',
+      },
+
+      activeClass: true,
+
+      isActive: true,
+      
     };
   },
-}); 
+
+  // methods()
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    }
+  },
+});
