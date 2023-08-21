631eafd6cc12c407b266ca3b334f1b9bc714b759
Xinan <youkingan11@gmail.com>
2023-08-21 14:36:24 +0100

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+      <header>
+        <h1>My Product Page</h1>
+      </header>
+      <main>
+        <ProductDisplay />
+        <section id="reviews">
+          <ReviewList />
+          <ReviewForm />
+        </section>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  // Import components here
+  import ProductDisplay from './components/ProductDisplay.vue'
+  import ReviewForm from './components/ReviewForm.vue'
+  import ReviewList from './components/ReviewList.vue'
+    
+  export default {
+    components: {
+        ProductDisplay,
+        ReviewList,
+        ReviewList
+    },
+    // Add your Vue logic here
+  }
+  </script>
+  
+  <style scoped>
+    /* Add your styles here */
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import ProductList from './components/ProductList.vue'
-import ProductDetails from './components/ProductDetails.vue'
-import ProductReview from './components/ProductReview.vue'
-import ProductReviewForm from './components/ProductReviewForm.vue'
-
 
 const app = Vue.createApp({
   // data()
